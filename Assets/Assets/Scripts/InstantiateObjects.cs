6afe44ab0c76c41974b4f03c48af6c07b598101d
ozanozkan <ozanozkan@gmail.com>
2015-05-12 16:50:10 +0300

    Galaxy and Star scrolling system v1. Collider to the bottom for destroying unused objects. GameObject scroller script for galaxies, stars, etc.

--- Assets/Assets/Scripts/InstantiateObjects.cs
+++ Assets/Assets/Scripts/InstantiateObjects.cs
@@ -4,30 +4,34 @@ using System.Collections;
 public class InstantiateObjects : MonoBehaviour {
 
 	public GameObject[] galaxies;
-	private GameObject currentGalaxy;
+	public GameObject[] stars;
 	public Vector3 vector;
 	public float moveSpeed;
 
 	// Use this for initialization
 	void Start () {
 		Invoke("SpawnGalaxy", 20);
+		Invoke("SpawnStar", 5);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (currentGalaxy != null){
-			currentGalaxy.transform.Translate (-Vector2.up * moveSpeed * Time.deltaTime);
 
-			if(currentGalaxy.transform.position.y <= -18)
-				Destroy(currentGalaxy);
-		}
 	}
 
 	void SpawnGalaxy(){
-		vector = new Vector3 (Random.Range(-12,12), 18 , 0);
-		var rnd = Random.Range (0, 3);
-		currentGalaxy = (GameObject)Instantiate (galaxies[rnd], vector, Quaternion.identity);
+		vector = new Vector3 (Random.Range(-12,12), 12 , 0);
+		var rnd = Random.Range (0, 0);
+		Instantiate (galaxies[rnd], vector, Quaternion.identity);
 
 		Invoke("SpawnGalaxy", 20);
+	}
+
+	void SpawnStar(){
+		vector = new Vector3 (Random.Range(-12,12), 12 , 0);
+		var rnd = Random.Range (0, 2);
+		Instantiate (stars[rnd], vector, Quaternion.identity);
+		
+		Invoke("SpawnStar", 5);
 	}
 }
---
+++ Assets/Assets/Scripts/ScrollController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (-Vector2.up * 2 * Time.deltaTime);
+	}
+}
