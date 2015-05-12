16a229067731176ec923fa3f4580ad32e646f6b1
ozanozkan <ozanozkan@gmail.com>
2015-05-12 14:13:16 +0300

    Galaxy feature added. v1.

--- Assets/Assets/Scripts/InstantiateObjects.cs
+++ Assets/Assets/Scripts/InstantiateObjects.cs
@@ -4,24 +4,18 @@ using System.Collections;
 public class InstantiateObjects : MonoBehaviour {
 
 	public GameObject galaxy1;
+	public Vector3 vector;
+	public float moveSpeed;
 
 	// Use this for initialization
 	void Start () {
-
+		vector = new Vector3 (Random.Range(-15,15), 10 , 0);
+		galaxy1 = (GameObject)Instantiate (galaxy1, vector, Quaternion.identity);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		int GalaxyInitiate = Random.Range (0, 5);
-
-		if (GalaxyInitiate == 1) {
-			Vector3 vector = new Vector3 (Random.Range(-12,12), 10 , 0);
-			Instantiate (galaxy1, vector, Quaternion.identity);
-			var vel = galaxy1.rigidbody2D.velocity;
-
-			while (vel.y >= -10){
-				vel.y =- 1;
-			}
-		}
+		galaxy1.transform.Translate (-Vector2.up * moveSpeed * Time.deltaTime);
+		// galaxy1.transform.position = Vector3.Lerp (galaxy1.transform.position, new Vector3 (galaxy1.transform.position.x, -10, galaxy1.transform.position.z), 0.1f);
 	}
 }
