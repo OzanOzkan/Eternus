9cfdcb1887eee1a9978cb11c95fe80e0b0c9bd4c
ozanozkan <ozanozkan@gmail.com>
2015-05-12 11:44:52 +0300

    Trying to fix the crashes.

---
+++ Assets/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroller : MonoBehaviour {
+
+	public float scrollSpeed;
+	private Vector2 savedOffset;
+
+	// Use this for initialization
+	void Start () {
+		savedOffset = renderer.sharedMaterial.GetTextureOffset ("_MainTex");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float y = Mathf.Repeat (Time.time * scrollSpeed, 1);
+		Vector2 offset = new Vector2 (0, y);
+		renderer.sharedMaterial.SetTextureOffset ("_MainTex", offset);
+	}
+
+	void OnDisable(){
+		renderer.sharedMaterial.SetTextureOffset ("_MainTex", savedOffset);
+	}
+}
---
+++ Assets/Scripts/InstantiateObjects.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantiateObjects : MonoBehaviour {
+
+	public GameObject galaxy1;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		int GalaxyInitiate = Random.Range (0, 5);
+
+		if (GalaxyInitiate == 1) {
+			Vector3 vector = new Vector3 (Random.Range(-12,12), 10 , 0);
+			Instantiate (galaxy1, vector, Quaternion.identity);
+			var vel = galaxy1.rigidbody2D.velocity;
+
+			while (vel.y >= -10){
+				vel.y =- 1;
+			}
+		}
+		*/
+	}
+}
